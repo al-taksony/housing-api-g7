2603c9b539f2326c5f07040665c7e26ad67afb97
Alberto Diaz <al.taksony@gmail.com>
2026-06-14 12:28:36 -0500

    Cambios en main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from ml_model import predict_price
 
-app = FastAPI()
+from database import engine
+from models import Base
+from routers import housing
+
+app = FastAPI(
+    title = "Housing API con FastAPI",
+    description = "API para predecir el precio de una vivienda usando Machine Learning, FastAPI y SQLAlchemy",
+    version = "1.0.0"
+)
+
+app.include_router(housing.router)
 
 #schema
 class Housing(BaseModel):
     rooms: int
 
 @app.get("/")
-def home():
-    return {"message": "Housing API"}
-
-@app.post("/housing_price")
-def housing_price(housing: Housing):
-    rooms = housing.rooms
-    price = predict_price(rooms)
+def index():
     return {
-        "rooms":rooms,
-        "price":price
+        "title": "FASTAPI HOUSING API VERSION 1.0.0",
+        "message": "Bienvenido a la API"
     }
